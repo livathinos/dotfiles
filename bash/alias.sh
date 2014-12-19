c2287aa02a85525748dff8efb00b237c0ed3a4c2
Spyros Livathinos <livathinos.spyros@gmail.com>
2014-12-19 09:57:37 +0100

    Remove .vim, add smart extract

--- bash/alias.sh
+++ bash/alias.sh
@@ -6,3 +6,5 @@ alias ....="cd ../../.."
 alias copyimg=copy_img
 alias be='bundle exec'
 alias rc='rails console'
+alias extract="!~/dotfiles/bin/extract"
+alias pr_stats="ruby ~/dotfiles/bin/pr_stats.rb"
