d45bebfd095755ec2a3a5014e30ed01b28ae57ed
Spyros Livathinos <livathinos.spyros@gmail.com>
2015-10-12 12:19:20 +0200

    Add fasd and init code

--- bash/alias.sh
+++ bash/alias.sh
@@ -9,3 +9,12 @@ alias rc='rails console'
 alias gs="git status -sb"
 alias extract="!~/dotfiles/bin/extract"
 alias pr_stats="ruby ~/dotfiles/bin/pr_stats.rb"
+
+alias a='fasd -a'        # any
+alias s='fasd -si'       # show / search / select
+alias d='fasd -d'        # directory
+alias f='fasd -f'        # file
+alias sd='fasd -sid'     # interactive directory selection
+alias sf='fasd -sif'     # interactive file selection
+alias z='fasd_cd -d'     # cd, same functionality as j in autojump
+alias zz='fasd_cd -d -i' # cd with interactive selection
--- bash/env.sh
+++ bash/env.sh
@@ -20,3 +20,5 @@ fi
 export ACKRC=".ackrc"
 export BUNDLER_EDITOR=vim
 export DOCKER_HOST=tcp://192.168.42.45:2375
+
+eval "$(fasd --init auto)"
